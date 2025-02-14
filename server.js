8eec51d0ea5dcdedcfcaefc11d1dfddff0a50250
Pedro Terriquez <pedroterriquez@hotmail.com>
2025-02-13 20:56:59 -0600

    Feature: Adding login

--- server.js
+++ server.js
@@ -4,16 +4,23 @@ const app = express();
 const PORT = 3000;
 const taskRoutes = require('./routes/taskRoutes');
 const projectRoutes = require('./routes/projectRoutes');
+const authRoutes = require('./routes/authRoutes');
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`incoming req: ${req.method} ${req.url}`)
+  next();
+})
 app.use('/tasks', taskRoutes);
 app.use('/projects', projectRoutes);
+app.use('/auth', authRoutes);
+
 
 app.get("/", (req, res) => {
   res.send('hola');
 });
 
 app.listen(PORT, () => {
-  console.log('running');
+  console.log('START');
 })
